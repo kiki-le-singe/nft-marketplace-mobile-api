8049adf93de144560d48a10d2ffd7f63cc683ee6
Anthony Albertini <aalbertini95@gmail.com>
2024-07-22 17:34:17 +0200

    [WIP] implement User api

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from 'src/controllers/app/app.controller';
 import { AppService } from 'src/services/app/app.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
