07a4bed055602c921bfffefbf2efc8d4b7b2c095
Anthony Albertini <aalbertini95@gmail.com>
2024-07-22 18:27:08 +0200

    Add message to indicate where the app is running

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,8 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   await app.listen(3002);
+  console.log(
+    `Application is running on: \x1b[33m${await app.getUrl()}\x1b[0m`,
+  );
 }
 bootstrap();
