be4067b045e89be38ae7f91aa3581a2b3a5f4230
Anthony Albertini <aalbertini95@gmail.com>
2024-07-22 17:59:38 +0200

    implement serve static

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from 'src/controllers/app/app.controller';
 import { AppService } from 'src/services/app/app.service';
 import { UserModule } from '../user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../..', 'client'),
+      serveStaticOptions: { index: false },
+      exclude: ['/api/(.*)'],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
