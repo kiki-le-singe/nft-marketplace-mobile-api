fd04009da7216381a76f800b1e95128972e9e5cb
Anthony Albertini <aalbertini95@gmail.com>
2024-07-23 16:37:08 +0200

    [security] implement Rate Limiting

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { join } from 'path';
 import { AppController } from 'src/controllers/app/app.controller';
 import { AppService } from 'src/services/app/app.service';
@@ -14,6 +15,13 @@ import { UserModule } from '../user/user.module';
       serveStaticOptions: { index: false },
       exclude: ['/api/(.*)'],
     }),
+    // https://docs.nestjs.com/security/rate-limiting
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     UserModule,
   ],
   controllers: [AppController],
